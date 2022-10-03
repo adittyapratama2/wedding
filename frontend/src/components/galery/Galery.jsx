78135d9c3acec605ad07a6f4c5ae058f4512eed4
adittya0015 <adittyapratama2@gmail.com>
2022-10-03 07:43:54 +0700

    foto-foto

--- frontend/src/components/galery/Galery.jsx
+++ frontend/src/components/galery/Galery.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./galery.css";
+import IMG1 from "../../assets/images/gallery/gallery1.jpg";
 
 const Galery = () => {
-  return <section id="galery">Galery</section>;
+  return (
+    <section id="galery">
+      <br />
+      <h2>Our Gallery</h2>
+      <div className="container gallery__container">
+        <img src={IMG1} alt="" className="img-container" />
+      </div>
+    </section>
+  );
 };
 
 export default Galery;
